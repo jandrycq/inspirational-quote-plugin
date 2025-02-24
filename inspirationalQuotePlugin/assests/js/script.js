a8f74026d88b51075a4fe76b5018241646d4ca11
jandrycq <jaysssanz@gmail.com>
2025-02-24 15:39:49 -0500

    Eliminar carpeta inspirationalQuotePlugin

--- inspirationalQuotePlugin/assests/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('new-quote').addEventListener('click', function () {
-        fetch(iq_ajax.ajaxurl + '?action=get_quote')
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('quote-container').innerText = `"${data}"`;
-            });
-    });
-});
